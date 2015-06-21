a9cc2448d7f934f20644e59f221c7c4000ed78a3
sysfloat <jan@Jans-MacBook-Pro-2.local>
2015-06-21 21:11:14 +0200

    fixed bug in findString, new patching method
    the new patchStringsInFile seems to be a lot faster and gets the same
    job done. Didn’t really understand why he chose such a complex logic
    for this simple task.

--- Continuity Activation Tool.app/Contents/Resources/contitool.sh
+++ Continuity Activation Tool.app/Contents/Resources/contitool.sh
@@ -1239,40 +1239,14 @@ function patchStringsInFile() {
     local FILE="$1"
     local PATTERN="$2"
     local REPLACEMENT="$3"
-
     #Find all unique strings in FILE that contain the pattern 
-    STRINGS=$("${stringsPath}" "${FILE}" "${PATTERN}" | sort -u -r)
+    STRINGS=$("${stringsPath}" "${FILE}" "${PATTERN}" | awk -F" " '{print $1}' | sort -u -r)
 
     if [ "${STRINGS}" != "" ] ; then
         #echo "File '${FILE}' contain strings with '${PATTERN}' in them:"
-
-        for OLD_STRING in ${STRINGS} ; do
+        for OFFSET in ${STRINGS} ; do
             # Create the new string with a simple bash-replacement
-            NEW_STRING=${OLD_STRING//${PATTERN}/${REPLACEMENT}}
-
-            # Create null terminated ASCII HEX representations of the strings
-            OLD_STRING_HEX="$(echo -n "${OLD_STRING}" | $xxdPath -g 0 -u -ps -c 256)00"
-            NEW_STRING_HEX="$(echo -n "${NEW_STRING}" | $xxdPath -g 0 -u -ps -c 256)00"
-
-            if [ ${#NEW_STRING_HEX} -le ${#OLD_STRING_HEX} ] ; then
-                # Pad the replacement string with null terminations so the
-                # length matches the original string
-                while [ ${#NEW_STRING_HEX} -lt ${#OLD_STRING_HEX} ] ; do
-                    NEW_STRING_HEX="${NEW_STRING_HEX}00"
-                done
-
-                #now, replace every occurrence of OLD_STRING with NEW_STRING 
-                #echo -n "Replacing ${OLD_STRING} with ${NEW_STRING}... "
-                $hexdumpPath -ve '1/1 "%.2X"' "${FILE}" | \
-                $sedPath "s/${OLD_STRING_HEX}/${NEW_STRING_HEX}/g" | \
-                $xxdPath -r -p > "${FILE}.tmp"
-                SAVEMOD=$($statPath -r "$FILE" | $cutPath -f3 -d' ')
-                $chmodPath "${SAVEMOD}" "${FILE}.tmp"
-                $mvPath "${FILE}.tmp" "${FILE}"
-            else
-                echo "NOT OK. New string '${NEW_STRING}' is longer than old" \
-                     "string '${OLD_STRING}'. Skipping."
-            fi
+            printf "%s" $REPLACEMENT | dd of="$FILE" bs=1 seek="0x""$OFFSET" conv=notrunc >> /dev/null 2>&1
         done
     else
     	echo "NOT OK. No filepath given for the hacking. Aborting."
